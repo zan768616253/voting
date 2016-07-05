d2de04abc0f6b52bcab9926b2a0de6123949c5e3
zan768616253 <768616253@qq.com>
2016-07-05 20:07:54 +0800

    add actions

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,5 @@
+import Eventproxy from 'eventproxy';
+
 import helpers from '../helpers';
 
 const faceHelper = new helpers.FaceHelper();
@@ -8,7 +10,38 @@ export function setEntries() {
 		faceHelper.getFacePair().then((pair) => {
 			dispatch({ type: 'SET_ENTRIES', status: 'COMPLETED',  pair: pair});
 		}).catch((err) => {
-			dispatch({ type: 'SET_ENTRIES', status: 'ERROR', });
+			console.log(err.message);
+			dispatch({ type: 'SET_ENTRIES', status: 'ERROR' });
 		})
+	}
+}
+
+export const VOTE  = 'VOTE';
+export function vote() {
+	return function(dispatch) {
+		const ep = new Eventproxy();
+
+		ep.fail((err) => {
+			console.log(err.message);
+			dispatch({ type: 'VOTE', status: 'ERROR' });
+		});
+
+		ep.once('vote', () => {
+			faceHelper.getFacePair().then((pair) => {
+				console.log('vote.faceHelper.getFacePair ok');
+                dispatch({ type: 'VOTE', status: 'COMPLETED', pair: pair })
+			}).catch((err) => {
+	            console.log('vote.faceHelper.getFacePair err');
+	            emit('error', err);
+	        });
+		});
+
+	    faceHelper.vote(entry).then(() => {
+	        console.log('vote.faceHelper.vote ok');
+	        ep.emit('vote', true);
+	    }).catch((err) => {
+	        console.log('vote.faceHelper.vote err');
+	        ep.emit('error', new Error('faceHelper.vote err: ' + err.message));
+	    });
 	}
 }
--- src/reducer.js
+++ src/reducer.js
@@ -9,18 +9,19 @@ export default function reducer(state = INITIAL_STATE, action) {
     console.log('reducer action: ' + JSON.stringify(action));
     switch (action.type) {
         case 'SET_ENTRIES':
-            console.log('begin SET_ENTRIES');
+            console.log('begin SET_ENTRIES status: ' + action.status);
             if (action.status === 'COMPLETED') {
                 return state.set('pair', List(action.pair));
-            } else {
-                return state;
             }
         case 'STAT':
             console.log('begin STAT');
             break;
         case 'VOTE':
-            console.log('begin VOTE');
-            break;
+            console.log('begin VOTE status: ' + action.status);
+            if (action.status === 'COMPLETED') {
+                return state.updateIn('pair', List(action.pair));
+            }
+
     }
     return state;
 }
