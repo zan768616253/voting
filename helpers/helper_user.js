c10fab734309b03fb92d018652f219d914d17954
zan768616253 <768616253@qq.com>
2016-07-28 11:43:32 +0800

    fix bug

--- helpers/helper_user.js
+++ helpers/helper_user.js
@@ -26,20 +26,22 @@ class UserHelper {
 
 	encryptPassword (email, password, salt) {
 		const time = Math.floor(Math.hypot(email.length, salt.length) * 59 % 500) + 1000;
-		return EncryptionHelper.encrypt(password, salt+email, time);
+		console.log('UserHelper.encryptPassword time: %d', time);
+		const encrypted = encryptionHelper.encrypt(password, salt+email, time);
+		console.log('UserHelper.encryptPassword encrypted: %s', encrypted);
+		return encrypted;
 	}
 
 	createUser (email, password, name, length) {
 		return new Promise((resolve, reject) => {
-			//console.log('UserHelper.createUser email: %s', email);
-			//console.log('UserHelper.createUser password: %s', password);
-			//console.log('UserHelper.createUser name: %s', name);
-			//console.log('UserHelper.createUser length: %d', length);
+			console.log('UserHelper.createUser email: %s', email);
+			console.log('UserHelper.createUser password: %s', password);
+			console.log('UserHelper.createUser name: %s', name);
+			console.log('UserHelper.createUser length: %d', length);
 
 			const salt = encryptionHelper.generateSalt(Math.abs(length) % 50);
 			console.log('UserHelper.createUser salt: %s', salt);
 			const encryptedPassword = this.encryptPassword(email, password, salt);
-			console.log('UserHelper.createUser encryptedPassword: %s', encryptedPassword);
 
 			const user = new models.User();
 			user.email = email;
@@ -47,6 +49,7 @@ class UserHelper {
 			user.password = encryptedPassword;
 			user.salt = salt;
 
+			console.log('UserHelper.createUser before user.save');
 			user.save((err) => {
 				if (err) {
 					console.log('UserHelper.createUser err %s', err.message);
--- test/helpers/helper_user_spec.js
+++ test/helpers/helper_user_spec.js
@@ -15,18 +15,18 @@ describe('user helper test', () => {
 		email = faker.internet.email();
 		name = faker.internet.userName();
 		password = '12345678';
-		console.log('user helper test finish before');
 	});
 
 	it('create user', (done) => {
 		const length = random.integer(1, 2000);
-		console.log('create user email: %s', email);
-		console.log('create user password: %s', password);
-		console.log('create user name: %s', name);
-		console.log('create user length: %d', length);
+		//console.log('create user email: %s', email);
+		//console.log('create user password: %s', password);
+		//console.log('create user name: %s', name);
+		//console.log('create user length: %d', length);
 
-		userHelper.createUser(email. password, name, length).then((result) => {
-			expect(result).to.be.true;
+		userHelper.createUser(email, password, name, length).then((result) => {
+			console.log('create user result: %s', result);
+			expect(result).to.be.ok;
 			done();
 		})
 	})
